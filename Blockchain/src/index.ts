68b7e56e4afb168ca95f900bc78846eeaf66a4eb
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2021-12-27 03:41:28 +0900

    started blockchain

--- Blockchain/src/index.ts
+++ Blockchain/src/index.ts
@@ -1,20 +1,31 @@
-class Human{
-  public name:string;
-  public age:number;
-  public gender:string;
-  constructor(name:string,age:number,gender?:string){
-    this.name=name;
-    this.age=age;
-    this.gender=gender;
+
+//블록 structure
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
-}
+} 
 
-const dan = new Human("Dan",24,"male");
+const genesisBlock :Block = new Block(0,"222224124","","hello",1234);
 
-const sayHi = (person): string => {
-  return `${person.name} 넌${person.age}살이구나 ${person.gender}고.`;
-};
+let blockchain: [Block] = [genesisBlock]
 
-console.log(sayHi(dan));
+console.log(blockchain);
 
-export {}; 
+
+export {};
