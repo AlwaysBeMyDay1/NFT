ef958dc8152ce63079bd35e85e745afb8687327c
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2021-12-27 20:24:10 +0900

    static function

--- Blockchain/src/index.ts
+++ Blockchain/src/index.ts
@@ -1,3 +1,4 @@
+import * as CryptoJS from "crypto-js";
 
 //블록 structure
 class Block {
@@ -6,6 +7,15 @@ class Block {
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString;
+
   constructor(
     index: number,
     hash: string,
@@ -19,13 +29,15 @@ class Block {
     this.data = data;
     this.timestamp = timestamp;
   }
-} 
+}
 
-const genesisBlock :Block = new Block(0,"222224124","","hello",1234);
+const genesisBlock: Block = new Block(0, "222224124", "", "hello", 1234);
 
-let blockchain: [Block] = [genesisBlock]
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = () : Block[]=> blockchain;
 
+const getLatestBlock = () : Block=>blockchain[blockchain.length-1]
 
+const getNewTimestamp = () :number => Math.round(new Date().getTime()/1000);
 export {};
